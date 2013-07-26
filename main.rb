a1480994f3ff870e9eef37470e92f0ab8b38fd66
amicojeko <stefano@mikamai.com>
2013-07-26 19:43:49 +0200

    aggiunti suoni e media

--- main.rb
+++ main.rb
@@ -17,7 +17,15 @@ class Table
   INPUT_PINS  = {goal_a: 0, goal_b: 3, start: 4}
   OUTPUT_PINS = {led: 7}
   LED_STATES  = {:on => 1, :off => 0}
-  GOAL_SOUND  = 'horn.mp3'
+  GOAL_SOUND  = 'media/horn.mp3'
+  GOAL_SOUND_A  = 'media/goal_team_a.wav'
+  GOAL_SOUND_B  = 'media/goal_team_b.wav'
+  IDLE_SOUND  = 'media/idle.wav'
+  REGISTER_SOUND  = 'media/register.wav'
+  MATCH_START_SOUND  = 'media/match_start.wav'
+  MATCH_END_SOUND  = 'media/match_end.wav'
+  
+  IDLE_VIDEO  = 'media/Holly\ e\ Benji.flv'
   STATES      = {idle: 0, registration: 1, start_match: 2, match: 3, end_match: 4}
 
 
@@ -59,29 +67,41 @@ class Table
     check_pressed INPUT_PINS[:start], :message => 'match begins now', :sound => GOAL_SOUND, :on => :idle do
       self.state = STATES[:registration]
     end
-    check_pressed INPUT_PINS[:goal_a], :message => 'goal team a', :sound => GOAL_SOUND, :on => :match do
+    check_pressed INPUT_PINS[:goal_a], :message => 'goal team a', :sound => GOAL_SOUND_A, :on => :match do
       increase_score teams[0]
     end
-    check_pressed INPUT_PINS[:goal_b], :message => 'goal team b', :sound => GOAL_SOUND, :on => :match do
+    check_pressed INPUT_PINS[:goal_b], :message => 'goal team b', :sound => GOAL_SOUND_B, :on => :match do
       increase_score teams[1]
     end
     reset_pins
   end
 
+  # TO_DELETE: cagata fatta per debuggare il say
+  # def p(param)
+  #   say(param)
+  # end
+
   def wait_for_start
     if state_idle? and !@done
+      
+      #play_video IDLE_VIDEO
+      #fixme: se mettiamo il video si incasina un po tutto (non riesco a sopparlo :) )
       p "idle - please push start button"
+      play_sound IDLE_SOUND
       @done = true
     end
   end
 
   def register_players
     if state_registration?
+      #kill @video_pid #non funziona!!
       clear_players
       clear_teams
       p 'register players'
+      play_sound REGISTER_SOUND
       PLAYERS.times do |n|
         p "player #{n}:"
+        play_sound "media/player_#{n}.wav" 
         player = "player_#{n}"
         get_player player until send(player)
       end
@@ -107,6 +127,7 @@ class Table
     if team.score >= GOALS
       team.make_winner
       p "the winner is team #{team.id}"
+      play_sound "media/winner_team_#{team.id}.wav"
       self.state = STATES[:end_match]
     end
   end
@@ -114,6 +135,7 @@ class Table
   def start_match
     if state_start_match?
       p "match has started"
+      play_sound MATCH_START_SOUND
       self.state = STATES[:match]
     end
   end
@@ -121,6 +143,9 @@ class Table
   def end_match
     if state_end_match?
       p "match is over"
+      play_sound MATCH_END_SOUND
+      #TODO: dare il risultato finale
+      #p "the final result is..."
       self.state = STATES[:idle]
       @done = false
     end
@@ -178,7 +203,22 @@ class Table
   end
 
   def play_sound(sound)
-    fork { exec 'mpg123','-q', sound }
+    #@sound_pid = fork { exec 'mpg123','-q', sound }
+    #@sound_pid = fork { exec 'omxplayer -o local ' + sound }
+    #trovo che omxplayer sia piu performante di mpg123, e legge anche i wav, cosa che mpg123 non fa
+    #qui ho messo system perché alcuni suoni devono essere bloccanti (come le voci di inizio partita ecc...)
+    #e altri no (Come il suono del gol)
+    #TODO: aggiungere un parametro per decidere se il suono è bloccante o meno, o almeno trovare il modo di evitare che i suoni si sovrappongano
+    system 'omxplayer -o local ' + sound
+  end
+
+  def play_video(video)
+    @video_pid = fork { exec 'omxplayer -o local ' + video }
+  end
+
+  def say(text)
+    #@say_pid = fork { exec 'echo "' + text + '" | festival --tts'}
+    @say_pid = fork { exec 'espeak "' + text + '"'}
   end
 
   def lock
@@ -199,6 +239,10 @@ class Table
 
   def clear_players
     PLAYERS.times {|n| send "player_#{n}=", nil}
+  end
+
+  def kill(pid)
+    system 'kill -9 ' + pid.to_s
   end
 end
 
