4af481c236ab2da0b05b1ae41bbf2a4c37a524cd
andrea longhi <andrea@spaghetticode.it>
2013-07-26 11:05:31 +0200

    refactoring

--- gpio.rb
+++ gpio.rb
@@ -2,7 +2,6 @@ require 'wiringpi'
 
 class SerialPort
   def initialize
-
   end
 end
 
@@ -24,92 +23,101 @@ end
 class Table
   INPUT_PINS  = {goal_a: 0, goal_b: 3, start: 4}
   OUTPUT_PINS = {led: 7}
-  GOAL_SOUND  = "horn.mp3"
+  LED_STATES  = {:on => 1, :off => 0}
+  GOAL_SOUND  = 'horn.mp3'
 
-  def initialize()
-    @s = WiringPi::Serial.new('/dev/ttyAMA0',9600)
+  def initialize
     @serialBuffer = ''
+    @serial = WiringPi::Serial.new('/dev/ttyAMA0',9600)
     @io = WiringPi::GPIO.new(WPI_MODE_PINS)
-    @io.mode INPUT_PINS[:goal_a], INPUT
-    @io.mode INPUT_PINS[:goal_b], INPUT
-    @io.mode INPUT_PINS[:start], INPUT
-    @io.mode OUTPUT_PINS[:led], OUTPUT
-    @lock = false
+    start_inputs
+    start_outputs
+    unlock
   end
 
   def mainloop
     loop do
-      #leggo lo stato degli input
       @buttonstate = @io.readAll
-
       #RFID CHECK
-      while (@s.serialDataAvail > 0)
-        @serialBuffer += @s.serialGetchar.chr
-      end
-      if @serialBuffer.size > 0
-        p @serialBuffer
-        @serialBuffer = ""
-      end
-
+      check_serial
       #INIZIO PARTITA
-      if button_pressed? INPUT_PINS[:start]
-        @lock = true
-        led_on
-        p "inizio partita"
-        play_sound GOAL_SOUND
-      end
-
+      check_pressed INPUT_PINS[:start], :message => 'inizio partita', :sound => GOAL_SOUND
       #GOAL SQUADRA A
-      if button_pressed? INPUT_PINS[:goal_a]
-        @lock = true
-        led_on
-        p "gol squadra 1"
-        play_sound GOAL_SOUND
-      end
-
+      check_pressed INPUT_PINS[:goal_a], :message => 'gol squadra a', :sound => GOAL_SOUND
       #GOAL SQUADRA B
-      if button_pressed? INPUT_PINS[:goal_b]
-        @lock = true
-        led_on
-        p "gol squadra 2"
-        play_sound GOAL_SOUND
-      end
-
+      check_pressed INPUT_PINS[:goal_b], :message => 'gol squadra b', :sound => GOAL_SOUND
       #TUTTI GLI INPUT A RIPOSO
-      if INPUT_PINS.values.inject(1) {|r, key| r*@buttonstate[key] } == 1
-        led_off
-        @lock = false
-      end
-
+      reset_pins
       sleep 0.002
-    #LOOP END
     end
-  #MAINLOOP METHOD END
-  end
-
-  def led_on
-    led 1
-  end
-
-  def led_off
-    led 0
   end
 
   private
 
+  def check_serial
+    while @serial.serialDataAvail > 0
+      @serialBuffer += @serial.serialGetchar.chr
+    end
+    if @serialBuffer.size > 0
+      p @serialBuffer
+      @serialBuffer = ''
+    end
+  end
+
+  def check_pressed(pin, opts)
+    if button_pressed? INPUT_PINS[:goal_a]
+      lock
+      led :on
+      p opts[:message]
+      play_sound opts[:sound]
+    end
+  end
+
+  def reset_pins
+     if all_button_depressed?
+      led :off
+      unlock
+    end
+  end
+
+  def all_button_depressed?
+    INPUT_PINS.values.inject(1) { |r, value| r * @buttonstate[value] } == 1
+  end
+
   def button_pressed?(button)
-    @buttonstate[button] == 0 && @lock == false #bottone premuto
+     !locked? and @buttonstate[button] == 0 # bottone premuto
   end
 
   def led(state)
-    @io.write OUTPUT_PINS[:led], state
+    @io.write OUTPUT_PINS[:led], LED_STATES[state]
   end
 
   def play_sound(sound)
     fork { exec 'mpg123','-q', sound }
   end
-end
 
+  def lock
+    @lock = true
+  end
+
+  def unlock
+    @lock = false
+  end
+
+  def locked?
+    @lock
+  end
+
+  def start_inputs
+    @io.mode INPUT_PINS[:goal_a], INPUT
+    @io.mode INPUT_PINS[:goal_b], INPUT
+    @io.mode INPUT_PINS[:start],  INPUT
+  end
+
+  def start_outputs
+    @io.mode OUTPUT_PINS[:led],  OUTPUT
+  end
+end
 
 t = Table.new
 t.mainloop
