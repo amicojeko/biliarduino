f3b3b1404134345c34520fc3c6c317edd210e941
andrea longhi <andrea@spaghetticode.it>
2013-07-29 15:13:10 +0200

    add player specs

--- spec/lib/player_spec.rb
+++ spec/lib/player_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Player do
-  pending 'rspecs works'
+  let(:id)   { '33' }
+  let(:role) { Player::ROLES.first }
+  subject { Player.new(id, role) }
+
+  it { should be_a Player }
+
+  it 'raise an error when role is not valid' do
+    expect do
+      Player.new(id, :pivot)
+    end.to raise_error
+  end
 end
