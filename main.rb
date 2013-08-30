3b012b782a1070d40ba728e46a2a69f5a970110f
andrea longhi <andrea@spaghetticode.it>
2013-08-30 11:59:15 +0200

    refactoring

--- main.rb
+++ main.rb
@@ -2,33 +2,63 @@ require 'wiringpi'
 require 'omxplayer'
 
 require File.expand_path('../lib/player', __FILE__)
-require File.expand_path('../lib/team', __FILE__)
+require File.expand_path('../lib/team',   __FILE__)
+
+
+
+
+class InputPin
+  # lock_time is used to lock only this input for consecutive press events, while glock_time locks the inputs globally for given seconds
+  attr_reader :pin, :pressed_value, :lock_time, :locked
+
+  def initialize(pin, opts)
+    @pin           = pin
+    @lock_time     = opts[:lock_time]
+    @pressed_value = opts[:pressed_value]
+  end
+
+  def locked?
+    locked
+  end
+
+  def lock
+    @locked = true
+  end
+
+  def unlock
+    @locked = false
+  end
+end
+
+
+
 
 class Table
   # TODO much of these constants should go in a configuration file
-  DELAY       = 0.002
-  GOALS       = 4
+  MAX_GOALS   = 4
   PLAYERS     = 4
   GOAL_DELAY  = 3
+  DELAY       = 0.002
   GOLDEN_GOAL = false
-
-  INPUT_PINS  = {goal_a: 0, goal_b: 3, start: 4}
-  OUTPUT_PINS = {led: 7}
+  OUTPUT_PINS = {led: 7} # TODO the only output pin used now is for debugging glocked input state
   LED_STATES  = {:on => 1, :off => 0}
   STATES      = {idle: 0, registration: 1, start_match: 2, match: 3, end_match: 4}
+  INPUT_PINS  = {
+    goal_a: InputPin.new(0, :pressed_value => 1, :glock_time => 3),
+    goal_b: InputPin.new(3, :pressed_value => 1, :glock_time => 3),
+    start:  InputPin.new(4, :pressed_value => 0) # no locking here
+  }
 
-  IDLE_SOUND        = {:name => 'media/idle.wav'          ,:duration => 2}
-  GOAL_SOUND        = {:name => 'media/horn.mp3'          ,:duration => 2}
-  GOAL_SOUND_A      = {:name => 'media/goal_team_a.wav'   ,:duration => 2}
-  GOAL_SOUND_B      = {:name => 'media/goal_team_b.wav'   ,:duration => 2}
-  REGISTER_SOUND    = {:name => 'media/register.wav'      ,:duration => 2}
-  MATCH_START_SOUND = {:name => 'media/match_start.wav'   ,:duration => 2}
-  MATCH_END_SOUND   = {:name => 'media/match_end.wav'     ,:duration => 2}
-  WINNER_TEAM_A     = {:name => "media/winner_team_a.wav" ,:duration => 2}
-  WINNER_TEAM_B     = {:name => "media/winner_team_b.wav" ,:duration => 2}
-
-  IDLE_VIDEO  = 'media/Holly\ e\ Benji.flv'
-
+  IDLE_SOUND        = {:name => 'media/idle.wav'          , :duration => 2}
+  START_SOUND       = {:name => 'media/horn.mp3'          , :duration => 2}
+  GOAL_SOUND_A      = {:name => 'media/goal_team_a.wav'   , :duration => 2}
+  GOAL_SOUND_B      = {:name => 'media/goal_team_b.wav'   , :duration => 2}
+  REGISTER_SOUND    = {:name => 'media/register.wav'      , :duration => 2}
+  MATCH_START_SOUND = {:name => 'media/match_start.wav'   , :duration => 2}
+  MATCH_END_SOUND   = {:name => 'media/match_end.wav'     , :duration => 2}
+  WINNER_TEAM_A     = {:name => "media/winner_team_a.wav" , :duration => 2}
+  WINNER_TEAM_B     = {:name => "media/winner_team_b.wav" , :duration => 2}
+  IDLE_VIDEO        = 'media/Holly\ e\ Benji.flv'
 
 
   attr_reader   :gpio
@@ -36,16 +66,15 @@ class Table
   PLAYERS.times { |n| attr_accessor "player_#{n}" }
 
 
-
   def initialize
     @gpio  = WiringPi::GPIO.new(WPI_MODE_PINS)
-    @omx = Omxplayer.instance
+    @omx   = Omxplayer.instance
     @teams = []
     set_goal_time
     init_serial
     init_inputs
     init_outputs
-    unlock
+    unglock
   end
 
   def mainloop
@@ -55,7 +84,7 @@ class Table
       # register_players
       start_match
       end_match
-      check_buttons
+      check_input_pins
       sleep DELAY
     end
   end
@@ -73,46 +102,44 @@ class Table
 
   private
 
-  def check_buttons
-    check_pressed INPUT_PINS[:start], :message => 'match begins now', :sound => GOAL_SOUND, :on => :idle, :pressed_value => 0 do
+  def check_input_pins
+    check_pressed INPUT_PINS[:start], :message => 'match begins now', :sound => START_SOUND, :on_state => :idle do
       set_state :registration
     end
-    check_pressed INPUT_PINS[:goal_a], :message => 'goal team a', :sound => GOAL_SOUND_A, :on => :match, :pressed_value => 1 do
+    check_pressed INPUT_PINS[:goal_a], :message => 'goal team a', :sound => GOAL_SOUND_A, :on_state => :match do
       increase_score teams[0]
     end
-    check_pressed INPUT_PINS[:goal_b], :message => 'goal team b', :sound => GOAL_SOUND_B, :on => :match, :pressed_value => 1 do
+    check_pressed INPUT_PINS[:goal_b], :message => 'goal team b', :sound => GOAL_SOUND_B, :on_state => :match do
       increase_score teams[1]
     end
-    reset_pins
+    reset_input_pins
   end
 
   def wait_for_start
-    if state_idle? and !@done
-      #play_video IDLE_VIDEO
-      #fixme: se mettiamo il video si incasina un po tutto (non riesco a sopparlo :) )
-      p_once "idle - please push start button"
+    if state_idle? and !@started
+      # fixme: se mettiamo il video si incasina un po tutto (non riesco a sopparlo :) )
+      # play_video IDLE_VIDEO
+      debug_once "idle - please push start button"
       play_sound IDLE_SOUND
-      @done = true
+      @started = true
     end
   end
 
   def register_players
     if state_registration?
-      #kill @video_pid #non funziona!!
-      clear_players
-      clear_teams
-      p 'register players'
+      clear_teams_and_players
+      debug 'register players'
       play_sound REGISTER_SOUND
       PLAYERS.times do |n|
-        p "player #{n}:"
-        play_sound({:name => "media/player_#{n}.wav", :duration => 2}) # TODO scompattare i suoni dei player
+        debug "player #{n}:"
+        play_sound :name => "media/player_#{n}.wav", :duration => 2 # TODO extract constants for these sounds
         player = "player_#{n}"
         get_player player until send(player)
       end
       teams << Team.new(:a)
       teams << Team.new(:b)
       set_state :start_match
-      puts self.state
+      debug self.state
     end
   end
 
@@ -130,17 +157,12 @@ class Table
     unless goal_already_registered?
       set_goal_time
       team.score += 1
-      # FIXME: non so se va qua, è la procedura per fare la foto
-      get_snapshot team.id
-      p "score is #{team.score} for team #{team.id}"
-      if team.score >= GOALS
-        team.set_winner
-        p "the winner is team #{team.id}"
-        sound = team.id == :a ? WINNER_TEAM_A : WINNER_TEAM_B # FIXME this is crap
-        play_sound sound
-        set_state :end_match
+      get_snapshot team
+      debug "team #{team.id} score: #{team.score}"
+      if team.score >= MAX_GOALS
+        finalize_match(team)
       end
-      unlock
+      unglock # trouble...
     else
       puts 'goal still already registered'
     end
@@ -154,7 +176,7 @@ class Table
       $A = true
     end
     if state_start_match?
-      p "match has started"
+      debug "match has started"
       play_sound MATCH_START_SOUND
       set_state :match
     end
@@ -162,10 +184,10 @@ class Table
 
   def end_match
     if state_end_match?
-      p "match is over"
+      debug "match is over"
       play_sound MATCH_END_SOUND
       # TODO: dare il risultato finale
-      # p "the final result is team a: #{teams.first.score}, team b: #{teams.last.score}"
+      debug "the final result is team a: #{teams.first.score}, team b: #{teams.last.score}"
       set_state :idle
     end
   end
@@ -181,7 +203,7 @@ class Table
   end
 
   def init_inputs
-    INPUT_PINS.values.each { |pin| gpio.mode(pin, INPUT) }
+    INPUT_PINS.each { |pin| gpio.mode(pin.pin, INPUT) }
   end
 
   def init_outputs
@@ -189,29 +211,30 @@ class Table
   end
 
   def check_pressed(pin, opts)
-    if button_pressed? pin, opts[:pressed_value]
-      if !opts[:on] or opts[:on] && send("state_#{opts[:on]}?")
-        lock
-        led :on
-        p opts[:message]
+    if pin_pressed? pin
+      # true when state is missing (callback happens always), or is correct for this event
+      if !opts[:on_state] or opts[:on_state] && send("state_#{opts[:on_state]}?")
+        glock
+        debug opts[:message]
         play_sound opts[:sound]
         yield if block_given?
       end
     end
   end
 
-  def all_button_released?
-    INPUT_PINS.values.inject(1) { |r, value| r * @buttonstate[value] } == 1
+  def any_pin_pressed?
+    INPUT_PINS.inject false do |bool, pin|
+      bool ||= @buttonstate[pin.pin] == pin.pressed_value
+    end
   end
 
-  def button_pressed?(button, pressed_value)
-    !locked? and @buttonstate[button] == pressed_value
+  def pin_pressed?(pin)
+    !glocked? and @buttonstate[pin.pin] == pin.pressed_value
   end
 
-  def reset_pins
-     if all_button_released?
-      led :off
-      unlock
+  def reset_input_pins
+     unless any_pin_pressed?
+      unglock
     end
   end
 
@@ -219,37 +242,23 @@ class Table
     gpio.write OUTPUT_PINS[:led], LED_STATES[state]
   end
 
-  def play_sound(sound)
-    @omx.open(sound[:name])
-    sleep sound[:duration]
+  # glock is global lock, locks all inputs. Each input can have its own lock, and its own input unlocker
+  def glock
+    led :on
+    @glock = true
   end
 
-  def play_video(video)
-    # @video_pid = fork { exec 'bin/play_media ' + video }
+  def unglock
+    led :off
+    @glock = false
   end
 
-  def say(text)
-    # @say_pid = fork { exec 'echo "' + text + '" | festival --tts'}
-    @say_pid = fork { exec 'espeak "' + text + '"'}
+  def glocked?
+    @glock
   end
 
-  def lock
-    @lock = true
-  end
-
-  def unlock
-    @lock = false
-  end
-
-  def locked?
-    @lock
-  end
-
-  def clear_teams
+  def clear_teams_and_players
     self.teams = []
-  end
-
-  def clear_players
     PLAYERS.times {|n| send "player_#{n}=", nil}
   end
 
@@ -257,13 +266,15 @@ class Table
     system "kill -9 #{pid}"
   end
 
-  def get_snapshot(camera)
-    fork { exec "fswebcam -r 640x480 -d /dev/video0 'snapshots/webcam#{Time.now.to_i}.jpg'"}
+  # FIXME prende un parametro, ma non viene usato
+  def get_snapshot(team)
+    camera = team.id
+    fork { exec "fswebcam -r 640x480 -d /dev/video0 'snapshots/webcam_#{Time.now.to_i}.jpg'"}
   end
 
-  def p_once(message)
+  def debug_once(message)
     unless @__printed
-      p message
+      debug message
       @__printed = true
     end
   end
@@ -274,8 +285,36 @@ class Table
 
   def goal_already_registered?
     enough_time_passed = last_goal_at + 3 >= Time.now
-    unlock if enough_time_passed
+    unglock if enough_time_passed
     enough_time_passed
+  end
+
+  private
+
+  def debug(message)
+    p message
+  end
+
+  def finalize_match(team)
+    team.set_winner
+    debug "the winner is team #{team.id}"
+    play_sound sound self.class.const_get "WINNER_TEAM_#{team.id}"
+    set_state :end_match
+  end
+
+  def play_sound(sound)
+    @omx.open(sound[:name])
+    sleep sound[:duration]
+  end
+
+  def play_video(video)
+    # TODO temporarily disabled
+    # @video_pid = fork { exec 'bin/play_media ' + video }
+  end
+
+  def say(text)
+    # @say_pid = fork { exec 'echo "' + text + '" | festival --tts'}
+    @say_pid = fork { exec 'espeak "' + text + '"'}
   end
 end
 
