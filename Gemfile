1bfb4f5ad29cc542e5ade2a43d0bcc3c6a4828b7
amicojeko <stefano@mikamai.com>
2013-09-10 14:15:22 +0200

    RFID library added

--- Gemfile
+++ Gemfile
@@ -8,3 +8,4 @@ group :development, :test do
   gem 'rb-fsevent', '~> 0.9'
   gem 'wiringpi_kb_adapter', :git => 'https://github.com/spaghetticode/wiringpi_kb_adapter'
 end
+
---
+++ lib/rfid.rb
@@ -0,0 +1,42 @@
+require "serialport"
+
+#params for serial port
+
+
+class RfidReader
+
+	def initialize (opts = {})
+    port_str  = opts[:port_str]   || "/dev/ttyAMA0"
+    baud_rate = opts[:baud_rate]  || 9600
+    data_bits = opts[:data_bits]  || 8
+    stop_bits = opts[:stop_bits]  || 1
+    parity    = opts[:parity]     || SerialPort::NONE
+    @sleeptime = opts[:sleeptime] || 0.2
+    
+    @sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+		@sp.sync = false
+		@sp.flush
+		@sp.read_timeout = -1
+	end
+
+	def read
+			reading = ""
+			puts "waiting for input"
+			#just read forever
+			while true do
+				if(i = @sp.gets)
+					reading = i.force_encoding("UTF-8").split("\x02")[1].split("\x03")[0] rescue ""
+					if reading.length == 12
+						@sp.flush
+						return reading 
+					end
+				end
+				sleep(@sleeptime)
+			end
+	end
+end
+
+
+# EXAMPLE:
+# @rfid = RfidReader.new(port_str, baud_rate, data_bits, stop_bits, parity)
+# p @rfid.read
