0247572166bede3ac06f689f1e087c1a79c73de3
andrea longhi <andrea@spaghetticode.it>
2013-09-11 09:21:58 +0200

    cleanup

--- lib/rfid.rb
+++ lib/rfid.rb
@@ -1,10 +1,9 @@
 require "serialport"
 
-#params for serial port
-
-
+# USAGE:
+# @rfid = RfidReader.new(port_str, baud_rate, data_bits, stop_bits, parity)
+# p @rfid.read
 class RfidReader
-
 	def initialize (opts = {})
     port_str  = opts[:port_str]   || "/dev/ttyAMA0"
     baud_rate = opts[:baud_rate]  || 9600
@@ -12,7 +11,7 @@ class RfidReader
     stop_bits = opts[:stop_bits]  || 1
     parity    = opts[:parity]     || SerialPort::NONE
     @sleeptime = opts[:sleeptime] || 0.2
-    
+
     @sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 		@sp.sync = false
 		@sp.flush
@@ -20,23 +19,16 @@ class RfidReader
 	end
 
 	def read
-			reading = ""
-			puts "waiting for input"
-			#just read forever
-			while true do
-				if(i = @sp.gets)
-					reading = i.force_encoding("UTF-8").split("\x02")[1].split("\x03")[0] rescue ""
-					if reading.length == 12
-						@sp.flush
-						return reading 
-					end
+		reading = ""
+    loop do
+			if i = @sp.gets
+				reading = i.force_encoding('UTF-8').split("\x02")[1].split("\x03")[0] rescue ''
+				if reading.size == 12
+					@sp.flush
+					return reading
 				end
-				sleep(@sleeptime)
 			end
+			sleep @sleeptime
+		end
 	end
 end
-
-
-# EXAMPLE:
-# @rfid = RfidReader.new(port_str, baud_rate, data_bits, stop_bits, parity)
-# p @rfid.read
