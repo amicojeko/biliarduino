15691a08f05d816041aa3df6fb8e635a6867c84d
andrea longhi <andrea@spaghetticode.it>
2013-09-11 09:32:53 +0200

    refactoring

--- lib/rfid.rb
+++ lib/rfid.rb
@@ -1,30 +1,36 @@
 require "serialport"
 
 # USAGE:
-# @rfid = RfidReader.new(port_str, baud_rate, data_bits, stop_bits, parity)
-# p @rfid.read
-class RfidReader
-	def initialize (opts = {})
-    port_str  = opts[:port_str]   || "/dev/ttyAMA0"
-    baud_rate = opts[:baud_rate]  || 9600
-    data_bits = opts[:data_bits]  || 8
-    stop_bits = opts[:stop_bits]  || 1
-    parity    = opts[:parity]     || SerialPort::NONE
-    @sleeptime = opts[:sleeptime] || 0.2
+# p RfidReader.read(port_str, baud_rate, data_bits, stop_bits, parity)
 
-    @sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
-		@sp.sync = false
-		@sp.flush
-		@sp.read_timeout = -1
+class RfidReader
+  def self.read(opts={})
+    new(opts)
+  end
+
+	def initialize(opts)
+   @sleeptime = opts[:sleeptime] || 0.2
+    port_str   = opts[:port_str]  || "/dev/ttyAMA0"
+    baud_rate  = opts[:baud_rate] || 9600
+    data_bits  = opts[:data_bits] || 8
+    stop_bits  = opts[:stop_bits] || 1
+    parity     = opts[:parity]    || SerialPort::NONE
+
+    SerialPort.open port_str, baud_rate, data_bits, stop_bits, parity do |port|
+		  port.sync = false
+		  port.flush
+		  port.read_timeout = -1
+      read(port)
+    end
 	end
 
-	def read
-		reading = ""
+	def read(port)
+		reading = ''
     loop do
-			if i = @sp.gets
+			if i = port.gets
 				reading = i.force_encoding('UTF-8').split("\x02")[1].split("\x03")[0] rescue ''
 				if reading.size == 12
-					@sp.flush
+					port.flush
 					return reading
 				end
 			end
