e24e89f80a599698f36385413850f3367bebe5ef
andrea longhi <andrea@spaghetticode.it>
2013-11-19 10:31:04 +0100

    change config configuration

--- lib/social.rb
+++ lib/social.rb
@@ -1,13 +1,13 @@
 require 'twitter'
 require 'yaml'
 
-CONFIG = YAML.load_file File.expand_path('../../config.yml', __FILE__)
+SOCIAL_CONFIG = YAML.load_file File.expand_path('../../config/social.yml', __FILE__)
 
 Twitter.configure do |config|
-  config.consumer_key       = CONFIG['twitter_key']
-  config.consumer_secret    = CONFIG['twitter_secret']
-  config.oauth_token        = CONFIG['twitter_oauth_token']
-  config.oauth_token_secret = CONFIG['twitter_oauth_secret']
+  config.consumer_key       = SOCIAL_CONFIG['twitter_key']
+  config.consumer_secret    = SOCIAL_CONFIG['twitter_secret']
+  config.oauth_token        = SOCIAL_CONFIG['twitter_oauth_token']
+  config.oauth_token_secret = SOCIAL_CONFIG['twitter_oauth_secret']
 end
 
 class Social
