bdf4774d0d827f736880854d0dccb5798bc55362
andrea longhi <andrea@spaghetticode.it>
2013-11-26 17:42:47 +0100

    fix params

--- lib/server.rb
+++ lib/server.rb
@@ -33,9 +33,11 @@ module Server
   end
 
   def get_score_params(teams)
-    match: {
-      team_a_score: teams.first.score,
-      team_b_score: teams.last.score
+    {
+      match: {
+        team_a_score: teams.first.score,
+        team_b_score: teams.last.score
+      }
     }
   end
 
