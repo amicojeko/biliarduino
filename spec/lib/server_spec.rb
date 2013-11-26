0e50e0060d59c8c7428122a8206d4515bb4cb360
andrea longhi <andrea@spaghetticode.it>
2013-11-26 09:27:57 +0100

    add Server#domain

--- spec/lib/server_spec.rb
+++ spec/lib/server_spec.rb
@@ -5,5 +5,11 @@ describe Server do
 
   it { should respond_to :start_match }
   it { should respond_to :update_match }
-  it { should respond_to :end_match }
+  it { should respond_to :close_match }
+
+  describe '#domain' do
+    it 'returns expected string' do
+      Server.domain.should == 'http://192.168.0.1:3000'
+    end
+  end
 end
